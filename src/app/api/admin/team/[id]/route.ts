a965e39912058656e4f1c5964f300334ff6a14a3
Nick Davis <ndavis55862@gmail.com>
2025-07-28 21:45:28 -0700

    fix error on admin user delete api for deployment

--- src/app/api/admin/team/[id]/route.ts
+++ src/app/api/admin/team/[id]/route.ts
@@ -46,19 +46,23 @@ export async function PATCH(req: NextRequest, { params }: { params: { id: string
     }
 }
 
-export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+export async function DELETE(req: NextRequest) {
   const session = await auth();
   const user = session?.user as User;
-  const p = await params;
 
   if (!user || user.globalRole !== "ADMIN") {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
+  const url = new URL(req.url);
+  const id = url.pathname.split("/").pop(); // extract [id] from the URL path
+
+  if (!id) {
+    return NextResponse.json({ error: "Missing team ID" }, { status: 400 });
+  }
+
   try {
-    await prisma.team.delete({
-      where: { id: p.id },
-    });
+    await prisma.team.delete({where: { id }});
 
     return NextResponse.json({ success: true });
   } catch (error) {
