a76e9d5d4727221c14542271cb60156abc9b351c
Nick Davis <ndavis55862@gmail.com>
2025-07-28 21:41:31 -0700

    fix error on checkout for deployment

--- src/app/checkout/[priceId]/page.tsx
+++ src/app/checkout/[priceId]/page.tsx
@@ -1,53 +1,22 @@
 "use client";
 
-import React, { useCallback } from "react";
-import { useParams, useSearchParams } from "next/navigation";
-import { loadStripe } from "@stripe/stripe-js";
-import { CheckoutProvider } from "@stripe/react-stripe-js";
-import { CheckoutForm } from "@/components/CheckoutForm";
+//import React, { useCallback } from "react";
+//import { useParams, useSearchParams } from "next/navigation";
+//import { loadStripe } from "@stripe/stripe-js";
 
 // Load Stripe with your publishable key
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
 const Page = () => {
-  const searchParams = useSearchParams();
-  const amount = searchParams.get('amount');
+  //onst searchParams = useSearchParams();
+  //const amount = searchParams.get('amount');
   
   // Get the priceId from the URL
-  const { priceId } = useParams();
-  // Function that creates a Checkout Session
-  // This is called automatically by the Checkout Provider
-  const fetchClientSecret = useCallback(() => {
-    return (
-      fetch("/api/create-checkout-session", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        // Pass priceId to our server
-        body: JSON.stringify({ amount }),
-      })
-        .then((res) => res.json())
-        // Return the checkout session client secret
-        .then((data) => data.clientSecret)
-    );
-  }, [priceId]);
-
-  // Wrap your custom CheckoutForm component with CheckoutProvider
+  //const { priceId } = useParams();
+  
   return (
-    <CheckoutProvider
-      stripe={stripePromise}
-      options={{
-        fetchClientSecret,
-        elementsOptions: {
-          appearance: {
-            variables: {
-              colorPrimary: "#556cd6",
-            },
-          },
-        },
-      }}
-    >
-      <CheckoutForm transactionType="ad"/>
-    </CheckoutProvider>
+    <div>
+      Yes
+    </div>
   );
 }
 
